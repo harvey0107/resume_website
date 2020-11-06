deb0a73e35326c76c4c5f9ed18ed90163d3a9777
rachel yang <rachel@rachels-MacBook-Air.local>
2020-11-05 21:42:48 -0500

    11/5 commit

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+grunt.initConfig({
+  git_deploy: {
+    your_target: {
+      options: {
+        url: 'git@github.com:harvey0107/resume-website'
+      },
+      src: 'desktop/sei/resume_website'
+    },
+  },
+})
